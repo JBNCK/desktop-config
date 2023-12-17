773fe991e6e6c5a6edea58f9a61e52cbd27e8da7
Jan Biernacik <jan@biernacik.org>
2023-12-17 11:00:31 +0100

    complete

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 sudo cp -r src/* /
 sudo systemctl enable update-icons
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas
-
+sudo flatpak install org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
+sudo flatpak override --system --filesystem=xdg-config/gtk-3.0:ro --filesystem=xdg-config/gtkrc-2.0:ro --filesystem=xdg-config/gtk-4.0:ro --filesystem=xdg-config/gtkrc:ro
